576d314d3c5d94d421f5d3151fed9c2f191033a4
Billy Purdue <billypurdue@gmail.com>
2025-10-20 15:09:36 -0400

    added a script for non-successful Azure logins

---
+++ Get-Failed-AzureLogins.ps1
@@ -0,0 +1,48 @@
+﻿#install-module microsoft.graph
+. ./ipinfotoken.ps1
+Connect-MgGraph -Scopes "AuditLog.Read.All"
+$startDate = (Get-Date).AddDays(-4).ToString("yyyy-MM-ddTHH:mm:ssZ")
+$signIns = Get-MgAuditLogSignIn -Filter "createdDateTime ge $startDate" -All
+
+$successful = $signIns | Where-Object { (!$_.status.errorCode -eq 0) } # I wanted to see unsuccessful logins but didn't want to change all the names :)
+
+$uniqueIPs = $successful.ipAddress | Sort-Object -Unique
+
+$ipCache = @{}
+
+foreach ($ip in $uniqueIPs) {
+    if (-not $ipCache.ContainsKey($ip)) {
+        $url = "https://ipinfo.io/" + "$ip" + "?token=" + $ipInfoToken
+            $response = Invoke-RestMethod -Uri $url -Method Get
+            $ipCache[$ip] = $response
+            Start-Sleep -Milliseconds 250  # avoid rate limits
+    }
+}
+
+$enriched = $successful | ForEach-Object {
+    $geo = $ipCache[$_.ipAddress]
+    [PSCustomObject]@{
+        User   = $_.userPrincipalName
+        Date   = $_.createdDateTime
+        IP     = $_.ipAddress
+	Error  = $_.status.errorCode
+	Reason = $_.status.failureReason
+	Details = $_.status.additionalDetails
+        City   = $geo.city
+        State  = $geo.region
+        Country= $geo.country
+        Org    = $geo.org
+    }
+}
+
+$nonIndiana = $enriched | Where-Object { $_.State -ne "Indiana" }
+
+$timestamp = Get-Date -Format "yyyyMMdd_HHmmss"
+
+$orgname = get-mgorganization | select DisplayName
+
+$filename = "~/Documents/" + $orgname.DisplayName + "_FailedNonIndianaLogins_" + $timestamp + ".csv"
+
+$nonIndiana | Export-Csv $filename -NoTypeInformation
+
+Disconnect-MgGraph
